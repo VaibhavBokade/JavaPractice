94224db76e52cd4f805d3cdce97aec354ef354de
Vaibhav Bokade <vaibhav.bokade@perennalysis.com>
2024-03-13 17:01:08 +0530

    Today's practice code

---
+++ ProblemsStats/src/main/java/org/example/problem10/MaxOccurringCharacter.java
@@ -0,0 +1,27 @@
+package org.example.problem10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxOccurringCharacter {
+
+    public static void main(String[] args) {
+        String randomString = "exampleexampleexam";
+        Map<Character, Integer> charCounts = new HashMap<>();
+
+        for (char c : randomString.toCharArray()) {
+            charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
+        }
+        char maxChar = 0;
+        int maxCount = 0;
+        for (Map.Entry<Character, Integer> entry : charCounts.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxChar = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        System.out.println("Character with maximum occurrences: " + maxChar);
+        System.out.println("Count: " + maxCount);
+    }
+}
